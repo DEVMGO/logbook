6bf181efdaa418a6474e0424d9d74d8de767f6d0
Dev-Golmohammadi <92379327+DEVMGO@users.noreply.github.com>
2022-07-09 15:20:18 +0430

    delete fontawesom

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import "../public/font/fontawesome-pro-5.15.1-web-ulabs/css/all.min.css";
+// import "../public/font/fontawesome-pro-5.15.1-web-ulabs/css/all.min.css";
 import "../node_modules/slick-carousel/slick/slick.css";
 import "../node_modules/slick-carousel/slick/slick-theme.css";
 import Layout from "../components/layout/layout";
